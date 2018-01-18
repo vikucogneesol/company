9ed6508feff3a2e4c1318fe0642f7d3413116f18
testdemo <vikram@cogneesol.com>
2018-01-18 17:01:04 +0530

    asdfadasd

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -17,6 +17,6 @@ if ( !isset($wp_did_header) ) {
 
 	// Load the theme template.
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
-	print "Hello Sandeep Salil";
+	print "Hello Sandeep yyyy";
 
 }
